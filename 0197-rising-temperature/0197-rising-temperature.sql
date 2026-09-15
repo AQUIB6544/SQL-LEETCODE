364d53fb73520f14c54556e8272c997c5420364f
MOHD AQUIB <mohdaquib195@gmail.com>
2026-09-15 20:25:08 +0530

    Time: 1280 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
+SELECT id
+FROM
+(SELECT id, temperature, recordDate,
+LAG(temperature) OVER(ORDER BY recordDate) AS previous_temperature,
+LAG(recordDate) OVER(ORDER BY recordDate) AS  previous_recordDate
+FROM Weather) t
+WHERE DATEDIFF(recordDate, previous_recordDate) = 1 AND temperature > previous_temperature
